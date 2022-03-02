fac7fa0d84357c450db46aff7a181d1584b5f834
konstapascal <pascalkonstantinos@gmail.com>
2022-03-02 12:46:50 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  title = 'noroff-assignment-7';
+}
