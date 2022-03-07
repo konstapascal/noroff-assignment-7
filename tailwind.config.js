ecad3433c5afa8d20b229991b5ada6acd731c622
Michael Zaar <michael.sh.zaar@gmail.com>
2022-03-07 21:04:34 +0100

    feat: reworked css to include official pokemon colouring

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    colors: { // Official Pokemon Colours
+      "light-blue": "#2a75bb", 
+      "dark-blue": "#3c5aa6",
+      "yellow": "#ffcb05",
+      "yellow-shadow": "#c7a008",
+      "boring-text": "#4A525A"
+    },
     extend: {},
   },
   plugins: [],
